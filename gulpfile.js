d908181fc3bf89014bff557d7af9be1518b58bb8
Carlos Montalvo-Ghigliotty <QuiQue@Mac-mini.localdomain>
2018-04-18 11:09:40 -0400

    modified content on gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,39 @@
 const gulp =  require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+//variables for paths
+
+const SOURCEPATH ={
+    sass:'src/scss/*.scss'
+}
+
+const SITEPATH = {
+    root: 'site/',
+    css: 'site/css',
+    js: 'site/js'
+}
+
+
 
 gulp.task('sass', function(){
-    return gulp.src('src/scss/site.scss')
+    return gulp.src(SOURCEPATH.sass)
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('site/css'));
+    .pipe(gulp.dest(SITEPATH.css));
+    
 });
 
-gulp.task('default',['sass']);
+gulp.task('serve', ['sass'], function(){
+    browserSync.init([SITEPATH.css +  '/*.css', SITEPATH.root + '/*.html', SITEPATH.js + '/*.js',],{
+        server:{
+            baseDir: SITEPATH.root
+        }
+    })
+});
+gulp.task('watch', ['sass','serve'], function(){
+    
+    gulp.watch([SOURCEPATH.sass], ['sass']);
+});
+
+gulp.task('default',['watch']);
