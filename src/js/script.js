c9564cb55f4da8ac7a3d2c4690c70d7176b51c61
Carlos Montalvo-Ghigliotty <QuiQue@Mac-mini.localdomain>
2018-04-19 19:51:42 -0400

    create script.js to call modules using browserify

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+global.$ = require('jquery');
+Bootstrap = require('bootstrap');
+mustache = require('mustache');
+
+$(document).ready(function(){
+    alert('jquery, bootstrap and mustache loaded');
+});
